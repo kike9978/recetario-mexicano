4479e97484c0823e4b833df089d6bb13ec96cf75
Kike <kike.mendoza9978@gmail.com>
2024-03-08 13:54:32 -0600

    Asjust design

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,10 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		fontFamily: {
+			"sans": ["Karla", "ui-sans-serif", "system-ui", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
+			"serif": ["'Playfair Display'", "ui-serif", "Georgia", "Cambria", "Times New Roman", "Times", "serif"]
+		}
 	},
 	plugins: [require('@tailwindcss/typography'),],
 }
